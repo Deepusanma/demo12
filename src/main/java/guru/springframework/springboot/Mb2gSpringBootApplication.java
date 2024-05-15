6c5199120fd5c3dc910ee6d2d84b997b43e4c1a4
ARUN HIRUTHIK K P <86754961+ARUN6383@users.noreply.github.com>
2024-05-15 17:13:34 +0530

    Update Mb2gSpringBootApplication.java

--- src/main/java/guru/springframework/springboot/Mb2gSpringBootApplication.java
+++ src/main/java/guru/springframework/springboot/Mb2gSpringBootApplication.java
@@ -3,7 +3,7 @@ package guru.springframework.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+
 public class Mb2gSpringBootApplication {
 
 	public static void main(String[] args) {
