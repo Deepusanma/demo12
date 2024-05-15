9e2ed10e3556cce749362cc9ba52b057e699eaed
ARUN HIRUTHIK K P <86754961+ARUN6383@users.noreply.github.com>
2024-05-15 17:05:48 +0530

    Update Mb2gSpringBootApplication.java
    changes in main classs

--- src/main/java/guru/springframework/springboot/Mb2gSpringBootApplication.java
+++ src/main/java/guru/springframework/springboot/Mb2gSpringBootApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Mb2gSpringBootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Mb2gSpringBootApplication.class, args);
+		SpringApplication.run(Mb2gSpringBootApplication.class);
 	}
 
 }
