7bc5e789780e0d25841983610ea69efef4f57203
ARUN HIRUTHIK K P <86754961+ARUN6383@users.noreply.github.com>
2024-05-16 11:06:36 +0530

    Removed annotation

--- src/main/java/guru/springframework/springboot/Mb2gSpringBootApplication.java
+++ src/main/java/guru/springframework/springboot/Mb2gSpringBootApplication.java
@@ -3,10 +3,9 @@ package guru.springframework.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class Mb2gSpringBootApplication {
 
-	public static void main(String[] args) {
+	public static void main(String) {
 		SpringApplication.run(Mb2gSpringBootApplication.class, args);
 	}
 
