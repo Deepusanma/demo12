4cb07604a40af58c4978b2770bad35e73d48b847
ARUN HIRUTHIK K P <86754961+ARUN6383@users.noreply.github.com>
2024-05-22 12:04:36 +0530

    request

--- src/main/java/guru/springframework/springboot/Mb2gSpringBootApplication.java
+++ src/main/java/guru/springframework/springboot/Mb2gSpringBootApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Mb2gSpringBootApplication{
 
-	public static void main(String[] args) {
+	public static void main(String) {
 		SpringApplication.run(Mb2gSpringBootApplication.class,args);
 	}
 
